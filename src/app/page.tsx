dccc53535d084533925d2ed42212ca994c1870eb
vineet <vineet@ovou.com>
2024-06-18 22:19:38 -0700

    minor refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
+import React from 'react';
 import WeatherApp from '../components/WeatherApp';
-// import '../styles/globals.css';
 import '../styles/styles.scss';
-
 
 export default function Home() {
   return (
-   <>
-     <WeatherApp />
-   </>
+    <>
+      <WeatherApp />
+    </>
   );
 }
